e228e9b451b43f3c0dfae9ce25291740198c83c9
Olorunyomi69 <isaacolorunyomi69@gmail.com>
2023-01-08 01:38:48 +0100

    no task message updated

--- src/App.js
+++ src/App.js
@@ -1,66 +1,67 @@
-import { useState } from 'react'
-import Header from './Header';
-import Tasks from './Tasks';
-import AddTask from './AddTask'
+import { useState } from "react";
+import Header from "./Header";
+import Tasks from "./Tasks";
+import AddTask from "./AddTask";
 
 function App() {
-  const [ showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
 
   const [tasks, setTasks] = useState([
     {
-       id: 1,
-       text: "Doctors appointment",
-       day: 'february 5th at 2:30pm',
-       reminder: true
-   },
-   {
-       id: 2,
-       text: "Meeting at School",
-       day: 'february 6th at 1:30pm',
-       reminder: true
-   },
-   {
-       id: 3,
-       text: "Food shopping",
-       day: 'february 7th at 2:30pm',
-       reminder: false
-   }
+      id: 1,
+      text: "Doctors appointment",
+      day: "february 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "february 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      day: "february 7th at 2:30pm",
+      reminder: false,
+    },
+  ]);
 
-]) 
+  // TO ADD THE TASK
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
-// TO ADD THE TASK
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
-}
+  //TO DELETE TASK
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-
-//TO DELETE TASK 
-const deleteTask = (id) => {
- setTasks(tasks.filter((task) => task.id !== id))
-}
-
-// FOR THE TOGGLE REMINDER
-const toggleReminder = (id) => {
-setTasks(tasks.map((task) => task.id === id
- ? { ...task, reminder: !task.reminder }: task))
-}
-
+  // FOR THE TOGGLE REMINDER
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="Container">
-      <Header onAdd= {() => setShowAddTask(!showAddTask)}
-      showAdd = {showAddTask}/>
-      {showAddTask && <AddTask onAdd = {addTask}/>}
-      {tasks.length > 0 ? ( 
-      <Tasks tasks= { tasks }  onDelete = { deleteTask } onToggle = {toggleReminder}/>)
-       :(
-         "An idle man is the devil's workshop; get something to do"
-       )}
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "The best time to plant a tree was 20 years ago. The second best time is now. - Chinese Proverb"
+      )}
     </div>
   );
 }
-
 
 export default App;
